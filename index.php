8a8be23c9db535442b2dd6877b6d3940f4f3b58d
ThomasLaforge <thomas.laforge.38@gmail.com>
2015-07-05 09:13:19 +0200

    initial

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Jeux2cartes - Index</title>
+  </head>
+  <body>
+    <header>
+      <h1>Jeux2cartes</h1>
+      <p>
+        Cette petite application va me permettre de réaliser un laboratoire
+        pour tester ma propre librairie de gestion de jeu de carte en javascript
+        avec la norme ECMAScript 6.
+      </p>
+    </header>
+
+    <div class="board">
+      <div class="deck" id="deck"></div>
+      <div class="defausse" id="defausse"></div>
+    </div>
+
+    <!-- Scripts javascript  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="js/board.js" charset="utf-8"></script>
+    <script src="js/deck.js" charset="utf-8"></script>
+    <script src="js/app.js" charset="utf-8"></script>
+  </body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  $('body').on('click', '#deck', function(){
+    if($(this).attr('active') == 'true'){
+      $(this).attr('active','false');
+      
+    }
+    else{
+      alert('Vous ne pouvez pas piocher de cartes.');
+    }
+  });
+
+
+});
