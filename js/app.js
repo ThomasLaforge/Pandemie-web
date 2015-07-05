1645911ebbf68d0356eec1e12e7a5fc84c215930
ThomasLaforge <thomas.laforge.38@gmail.com>
2015-07-05 09:47:11 +0200

    little change on app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ $(function(){
   $('body').on('click', '#deck', function(){
     if($(this).attr('active') == 'true'){
       $(this).attr('active','false');
-      
     }
     else{
       alert('Vous ne pouvez pas piocher de cartes.');
