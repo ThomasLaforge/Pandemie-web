114d9adacd9ff577f0a62373270d537cef16466a
ThomasLaforge <thomas.laforge.38@gmail.com>
2015-07-09 22:55:09 +0200

    Objet deck validé

--- build/app.js
+++ build/app.js
@@ -1,10 +1,22 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var $ = require('jQuery');
+$ = require('jquery');
+	require('./deck');
+console.log(new Deck());
 
-var deck = require('./deck');
-deck.init();
-},{"./deck":2,"jQuery":3}],2:[function(require,module,exports){
-exports.init = function (n) { $(body).append('test'); }
+// var mon_deck = new deck();
+// console.log(mon_deck.getParam());
+},{"./deck":2,"jquery":3}],2:[function(require,module,exports){
+'use strict';
+class Deck {
+  constructor() {
+    this._param = 'mon parametre';
+  }
+  getParam() {
+    return this._param;
+  }
+}
+
+module.export = Deck;
 },{}],3:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v2.1.4
--- js/deck.js
+++ js/deck.js
@@ -1,1 +1,11 @@
-exports.init = function (n) { $(body).append('test'); }
+'use strict';
+class Deck {
+  constructor() {
+    this._param = 'mon parametre';
+  }
+  getParam() {
+    return this._param;
+  }
+}
+
+module.export = Deck;
