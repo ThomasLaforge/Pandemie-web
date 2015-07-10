02413f69352bd90cd6ca892389079e351671d93f
ThomasLaforge <thomas.laforge.38@gmail.com>
2015-07-10 10:00:23 +0200

    merge

--- js/deck.js
+++ js/deck.js
@@ -1,18 +1,4 @@
 'use strict';
-<<<<<<< HEAD
-
-	class Point {
-        constructor(x, y) {
-            this.x = x;
-            this.y = y;
-        }
-        toString() {
-            console.log(this.x + ', ' + this.y);
-        }
-    }
-	
-export Point;
-=======
 class Deck {
   constructor() {
     this._param = 'mon parametre';
@@ -23,4 +9,3 @@ class Deck {
 }
 
 module.export = Deck;
->>>>>>> bbb7323d51879cae04148db69f9039878e960def
