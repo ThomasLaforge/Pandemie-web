7916ce636aaa96137c9935d6ce78c3d5d1176b97
ThomasLaforge <thomas.laforge.38@gmail.com>
2015-07-10 09:18:35 +0200

    test deck.js

--- js/deck.js
+++ js/deck.js
@@ -1,1 +1,13 @@
-exports.init = function (n) { $(body).append('test'); }
+'use strict';
+
+	class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        toString() {
+            console.log(this.x + ', ' + this.y);
+        }
+    }
+	
+export Point;
